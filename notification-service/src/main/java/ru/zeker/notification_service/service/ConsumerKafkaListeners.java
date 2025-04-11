47e94543956353e11cdfcc765d42986c6602949d
Zeker2908 <mud.runner@bk.ru>
2025-04-11 13:36:18 +0400

    Minor update

--- notification-service/src/main/java/ru/zeker/notification_service/service/ConsumerKafkaListeners.java
+++ notification-service/src/main/java/ru/zeker/notification_service/service/ConsumerKafkaListeners.java
@@ -1,13 +1,11 @@
-package ru.zeker.notification_service.component;
+package ru.zeker.notification_service.service;
 
-import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import ru.zeker.common.dto.UserRegisteredEvent;
-import ru.zeker.notification_service.service.EmailService;
 
 import java.util.List;
 
@@ -18,7 +16,7 @@ public class ConsumerKafkaListeners {
     private final EmailService emailService;
 
     @KafkaListener(topics = "user-registered-events", groupId = "notification-service")
-    void listenRegisteredEvents(List<ConsumerRecord<String, UserRegisteredEvent>> records) throws MessagingException {
+    void listenRegisteredEvents(List<ConsumerRecord<String, UserRegisteredEvent>> records) {
         records.forEach(r ->{
             try {
                 log.info(
